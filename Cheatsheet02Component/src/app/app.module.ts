5ec3abbdf3044a0d3cccb2fe7c7ccb3ae8df5a17
taitruong <me@mr-t.org>
2019-02-11 16:14:06 +0100

    ch02 - new component my-list: ng g c my-list

--- Cheatsheet02Component/src/app/app.module.ts
+++ Cheatsheet02Component/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyListComponent } from './my-list/my-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyListComponent
   ],
   imports: [
     BrowserModule
---
+++ Cheatsheet02Component/src/app/my-list/my-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tai-my-list',
+  templateUrl: './my-list.component.html',
+  styles: []
+})
+export class MyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
